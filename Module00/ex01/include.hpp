5f0d34fa8f3be645166d6c0f2dcd5debc3118070
fkoolhov <fkoolhov@student.codam.nl>
2023-06-06 18:58:05 +0200

    Implemented classes in Module00 ex01

--- Module00/ex01/include.hpp
+++ Module00/ex01/include.hpp
@@ -6,9 +6,29 @@
 /*   By: felicia <felicia@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/06 13:05:53 by felicia           #+#    #+#             */
-/*   Updated: 2023/06/06 13:05:55 by felicia          ###   ########.fr       */
+/*   Updated: 2023/06/06 18:56:36 by felicia          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iomanip>
 #include <string>
+#include <stdlib.h>
+#include <iostream>
+
+
+class Contact
+{
+	public:
+		int			index;
+		std::string	first_name;
+		std::string	last_name;
+		std::string	nickname;
+		std::string	phone_number;
+		std::string	darkest_secret;
+};
+
+class PhoneBook
+{
+	public:
+		Contact	array[8];
+};
--- Module00/ex01/main.cpp
+++ Module00/ex01/main.cpp
@@ -6,13 +6,85 @@
 /*   By: felicia <felicia@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/06 13:05:04 by felicia           #+#    #+#             */
-/*   Updated: 2023/06/06 13:11:34 by felicia          ###   ########.fr       */
+/*   Updated: 2023/06/06 18:57:00 by felicia          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "include.hpp"
 
+void	search_for_specific_contact()
+{
+	std::cout << "You chose SEARCH.\n";
+}
+
+Contact	add_new_contact(Contact contact, int i)
+{
+	// Contact		contact;
+
+	std::cout << "You chose ADD.\n";
+	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+	contact.index = i;
+	std::cout << "Enter first name:\n";
+	std::getline(std::cin, contact.first_name, '\n');
+	std::cout << "Enter last name:\n";
+	std::getline(std::cin, contact.last_name, '\n');
+	std::cout << "Enter nickname:\n";
+	std::getline(std::cin, contact.nickname, '\n');
+	std::cout << "Enter phone number:\n";
+	std::getline(std::cin, contact.phone_number, '\n');
+	std::cout << "Enter darkest secret:\n";
+	std::getline(std::cin, contact.darkest_secret, '\n');
+	return (contact);
+}
+
+void	print_all_contacts(PhoneBook pb, int i)
+{
+	int j = 0;
+	std::cout << pb.array[j].first_name;
+	while (j <= i)
+	{
+		std::cout << "index is: " << pb.array[j].index << std::endl;
+		std::cout << "first name is: " << pb.array[j].first_name << std::endl;
+		std::cout << "last name is: " << pb.array[j].last_name << std::endl;
+		std::cout << "nickname is: " << pb.array[j].nickname << std::endl;
+		std::cout << "phone number is: " << pb.array[j].phone_number << std::endl;
+		std::cout << "darkest secret is: " << pb.array[j].darkest_secret << std::endl;
+		std::cout << std::endl;
+		j++;
+	}
+}
+
 int	main(void)
 {
+	Contact		contact;
+	std::string	command;
+	PhoneBook	pb;
+	int			i = 0;
+	
+
+	while (true)
+	{
+		std::cout << "Please choose a command: ADD, SEARCH or EXIT.\n";
+		std::cin >> command;
+		if (command.compare("ADD") == 0)
+		{
+			contact = pb.array[i];
+			pb.array[i] = add_new_contact(contact, i);
+			print_all_contacts(pb, i);
+			i++;
+		}
+		else if (command.compare("SEARCH") == 0)
+		{
+			search_for_specific_contact();
+		}
+		else if (command.compare("EXIT") == 0)
+		{
+			std::cout << "Exiting phonebook.\n";
+			break ;
+		}
+		else
+			std::cout << "That command doesn't exist, please choose ADD, SEARCH or EXIT.\n";
+	}
+	
 	return (3);
 }
