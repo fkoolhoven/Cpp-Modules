aceaad64aec665c512ed4e4bee469782d8d5e8ac
fkoolhov <fkoolhov@student.codam.nl>
2023-06-12 18:11:59 +0200

    Search function working in Module00 ex01

--- Module00/ex01/main.cpp
+++ Module00/ex01/main.cpp
@@ -6,47 +6,56 @@
 /*   By: felicia <felicia@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/06 13:05:04 by felicia           #+#    #+#             */
-/*   Updated: 2023/06/07 22:19:58 by felicia          ###   ########.fr       */
+/*   Updated: 2023/06/12 18:11:12 by felicia          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "include.hpp"
 
+void	print_contact_info(std::string contact_info, int strlen)
+{
+	if (strlen > 10)
+		std::cout << contact_info.substr(0, 9) << ".|";
+	else
+		std::cout << std::string(10 - strlen, ' ') << contact_info << '|';
+}
+
 void	search_for_specific_contact(PhoneBook phonebook)
 {
-	std::cout << "You chose SEARCH.\n";
-	// Display the saved contacts as a list of 4 columns: index, first name, last
-	// name and nickname.
-	// ◦ Each column must be 10 characters wide. A pipe character (’|’) separates
-	// them. The text must be right-aligned. If the text is longer than the column,
-	// it must be truncated and the last displayable character must be replaced by a
-	// dot (’.’).
-	// ◦ Then, prompt the user again for the index of the entry to display. If the index
-	// is out of range or wrong, define a relevant behavior. Otherwise, display the
-	// contact information, one field per line.
 	int i;
+	int	strlen;
+
+	std::cout << "\nID        |FIRST NAME| LAST NAME|  NICKNAME|\n";
+	std::cout << std::string(44, '-') << std::endl;
 	for (i = 0; phonebook.contacts[i].index >= 0; i++)
 	{
-		std::cout << phonebook.contacts[i].index;
-		std::cout << phonebook.contacts[i].first_name;
-		std::cout << phonebook.contacts[i].last_name;
-		std::cout << phonebook.contacts[i].nickname << std::endl;
+		std::cout << phonebook.contacts[i].index << std::string(9, ' ') << '|';
+		strlen = phonebook.contacts[i].first_name.length();
+		print_contact_info(phonebook.contacts[i].first_name, strlen);
+		strlen = phonebook.contacts[i].last_name.length();
+		print_contact_info(phonebook.contacts[i].last_name, strlen);
+		strlen = phonebook.contacts[i].nickname.length();
+		print_contact_info(phonebook.contacts[i].nickname, strlen);
+		std::cout << std::endl;
 	}
+	std::cout << std::endl;
+	
 	std::string	id;
 	int			id_as_int;
 	std::cout << "Choose a contact ID.\n";
 	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 	std::getline(std::cin, id, '\n');
 	id_as_int = stoi(id);
-	std::cout << "index is: " << phonebook.contacts[id_as_int].index << std::endl;
-	std::cout << "first name is: " << phonebook.contacts[id_as_int].first_name << std::endl;
-	std::cout << "last name is: " << phonebook.contacts[id_as_int].last_name << std::endl;
-	std::cout << "nickname is: " << phonebook.contacts[id_as_int].nickname << std::endl;
-	std::cout << "phone number is: " << phonebook.contacts[id_as_int].phone_number << std::endl;
-	std::cout << "darkest secret is: " << phonebook.contacts[id_as_int].darkest_secret << std::endl;
-	std::cout << std::endl;
-
+	// what if invalid input
 	
+	std::cout << std::endl;
+	std::cout << "INDEX: " << phonebook.contacts[id_as_int].index << std::endl;
+	std::cout << "FIRST NAME: " << phonebook.contacts[id_as_int].first_name << std::endl;
+	std::cout << "LAST NAME: " << phonebook.contacts[id_as_int].last_name << std::endl;
+	std::cout << "NICKNAME: " << phonebook.contacts[id_as_int].nickname << std::endl;
+	std::cout << "PHONE NUMBER: " << phonebook.contacts[id_as_int].phone_number << std::endl;
+	std::cout << "DARKEST SECRET: " << phonebook.contacts[id_as_int].darkest_secret << std::endl;
+	std::cout << std::endl;
 }
 
 Contact	add_new_contact(Contact contact, int i)
