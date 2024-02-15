72edbab1fec9ee78d90ce38c3766976bb594e018
Felicia Koolhoven <fmkoolhoven@gmail.com>
2024-02-15 16:53:49 +0100

    Finished Module07/ex01

---
+++ Module07/ex01/src/main.cpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fkoolhov <fkoolhov@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/15 16:27:10 by fkoolhov          #+#    #+#             */
+/*   Updated: 2024/02/15 16:52:59 by fkoolhov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "iter.hpp"
+
+int main(void)
+{
+	std::cout << YELLOW BOLD "\nTesting with int array:\n" OFF;
+	int int_array[] = {1, 2, 3, 4, 5};
+	iter(int_array, 5, print_template);
+	std::cout << std::endl;
+
+	std::cout << YELLOW BOLD "\nTesting with char array:\n" OFF;
+	char char_array[] = {'a', 'b', 'c', 'd', 'e'};
+	iter(char_array, 5, print_template);
+	std::cout << std::endl;
+	
+	std::cout << YELLOW BOLD "\nTesting with string array:\n" OFF;
+	std::string string_array[] = {"Wow", "it's", "a", "string", "array!"};
+	iter(string_array, 5, print_template);
+	std::cout << std::endl;
+
+	std::cout << YELLOW BOLD "\nTesting with struct array:\n" OFF;
+	t_test_struct felicia = {"Felicia", "Koolhoven"};
+	t_test_struct mara = {"Mara", "Koolhoven"};
+	t_test_struct family[] = {felicia, mara};
+	iter(family, 2, print_template);
+	std::cout << std::endl << std::endl;
+
+	return (EXIT_SUCCESS);
+}
