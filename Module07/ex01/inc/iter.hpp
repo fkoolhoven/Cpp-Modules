9a6d4625200d29929b7b58437afe06cc1f9b3482
Felicia Koolhoven <fmkoolhoven@gmail.com>
2024-02-27 15:43:18 +0100

    Fixed const bug

--- Module07/ex01/inc/iter.hpp
+++ Module07/ex01/inc/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: fkoolhov <fkoolhov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/15 16:26:31 by fkoolhov          #+#    #+#             */
-/*   Updated: 2024/02/27 15:29:52 by fkoolhov         ###   ########.fr       */
+/*   Updated: 2024/02/27 15:42:31 by fkoolhov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,15 +19,15 @@
 
 # include <iostream>
 
-template <typename T>
-void iter(T *array, size_t length, void (*function)(T&))
+template <typename T, typename F>
+void iter(T *array, size_t length, F function)
 {
 	for (size_t i = 0; i < length; i++)
 		function(array[i]);
 };
 
 template <typename T>
-void print_template(T& t)
+void print_template(T t)
 {
 	std::cout << t << " ";
 };
--- Module07/ex01/src/main.cpp
+++ Module07/ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: fkoolhov <fkoolhov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/15 16:27:10 by fkoolhov          #+#    #+#             */
-/*   Updated: 2024/02/27 15:30:02 by fkoolhov         ###   ########.fr       */
+/*   Updated: 2024/02/27 15:42:54 by fkoolhov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,24 +28,24 @@ int main(void)
 {
 	std::cout << YELLOW BOLD "\nTesting with int array:\n" OFF;
 	int int_array[] = {1, 2, 3, 4, 5};
-	iter(int_array, 5, print_template);
+	iter(int_array, 5, print_template<int>);
 	std::cout << std::endl;
 
 	std::cout << YELLOW BOLD "\nTesting with char array:\n" OFF;
 	char char_array[] = {'a', 'b', 'c', 'd', 'e'};
-	iter(char_array, 5, print_template);
+	iter(char_array, 5, print_template<char>);
 	std::cout << std::endl;
 	
 	std::cout << YELLOW BOLD "\nTesting with string array:\n" OFF;
 	std::string string_array[] = {"Wow", "it's", "a", "string", "array!"};
-	iter(string_array, 5, print_template);
+	iter(string_array, 5, print_template<std::string>);
 	std::cout << std::endl;
 
 	std::cout << YELLOW BOLD "\nTesting with struct array:\n" OFF;
 	t_test_struct felicia = {"Felicia", "Koolhoven"};
 	t_test_struct mara = {"Mara", "Koolhoven"};
 	t_test_struct family[] = {felicia, mara};
-	iter(family, 2, print_template);
+	iter(family, 2, print_template<t_test_struct>);
 	std::cout << std::endl << std::endl;
 
 	return (EXIT_SUCCESS);
