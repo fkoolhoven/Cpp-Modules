fa392f652824d241b7439b1e93bb331ff6d64fed
Felicia Koolhoven <fmkoolhoven@gmail.com>
2024-02-28 16:53:45 +0100

    Finished Module08

--- Module08/ex02/inc/MutantStack.hpp
+++ Module08/ex02/inc/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: fkoolhov <fkoolhov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/20 17:46:17 by fkoolhov          #+#    #+#             */
-/*   Updated: 2024/02/28 13:36:40 by fkoolhov         ###   ########.fr       */
+/*   Updated: 2024/02/28 16:46:44 by fkoolhov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,29 +27,40 @@
 # define BOLD	"\033[1m"
 # define OFF	"\033[0m"
 
-template <typename T>
-class MutantStack : public std::stack<T>
+template <typename T, class deque = std::deque<T>>
+class MutantStack : public std::stack<T, deque>
 {
+	private:
+		typedef std::stack<T, deque> stack;
+	
 	public:
-		// Define stack and iterator type
-		typedef std::stack<T> stack;
-		typedef typename stack::container_type::iterator iterator;
+		typedef typename deque::iterator iterator;
 		
-		// Inherit constructors from std::stack
-		MutantStack() : stack() { std::cout << GREEN "MutantStack created\n" OFF;}
-		MutantStack(MutantStack const &other) : stack(other) { std::cout << GREEN "MutantStack copy created\n" OFF;}
-		~MutantStack() { this->c.clear(); std::cout << RED "MutantStack destroyed\n" OFF;}
-		
-		// Overloaded opperator
-		MutantStack &operator=(MutantStack &other)
-		{
-			stack::operator=(other);
-			return *this;
+		MutantStack() : stack() 
+		{ 
+			std::cout << GREEN "MutantStack created\n" OFF; 
 		}
-
-		// Add begin() and end() member functions to return iterators
-		iterator begin() { return this->c.begin(); }
-		iterator end() { return this->c.end(); }
+		MutantStack(MutantStack const& source) : stack(source) 
+		{ 
+			std::cout << GREEN "MutantStack copy created\n" OFF; 
+		}
+		~MutantStack() 
+		{ 
+			std::cout << RED "MutantStack destroyed\n" OFF; 
+		}
+		MutantStack& operator=(MutantStack &source)
+		{
+			stack::operator=(source);
+			return (*this);
+		}
+		iterator begin() 
+		{ 
+			return (this->c.begin()); 
+		}
+		iterator end() 
+		{ 
+			return (this->c.end()); 
+		}
 };
 
 #endif
--- Module08/ex02/src/main.cpp
+++ Module08/ex02/src/main.cpp
@@ -6,64 +6,114 @@
 /*   By: fkoolhov <fkoolhov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/20 17:46:07 by fkoolhov          #+#    #+#             */
-/*   Updated: 2024/02/20 18:09:18 by fkoolhov         ###   ########.fr       */
+/*   Updated: 2024/02/28 16:28:26 by fkoolhov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "MutantStack.hpp"
 
-void TestFromSubjectWithMutant(void)
+void TestFromSubjectWithMutantChar(void)
 {
-	std::cout << BOLD YELLOW "Test from subject with mutant stack\n" OFF;
+	std::cout << BOLD YELLOW "Test from subject with mutant stack (char)\n" OFF;
 
-	MutantStack<int> mstack;
-	mstack.push(5);
-	mstack.push(17);
-	std::cout << "Top: " << mstack.top() << std::endl;
-	mstack.pop();
-	std::cout << "Size: " << mstack.size() << std::endl;
-	mstack.push(3);
-	mstack.push(5);
-	mstack.push(737);
-	//[...]
-	mstack.push(0);
-	MutantStack<int>::iterator it = mstack.begin();
-	MutantStack<int>::iterator ite = mstack.end();
-	++it;
-	--it;
-	while (it != ite)
+	MutantStack<char> mutant_stack;
+	mutant_stack.push('a');
+	mutant_stack.push('Z');
+	std::cout << "Top: " << mutant_stack.top() << std::endl;
+	mutant_stack.pop();
+	std::cout << "Size: " << mutant_stack.size() << std::endl;
+	mutant_stack.push('Q');
+	mutant_stack.push('@');
+	mutant_stack.push('|');
+	mutant_stack.push('0');
+	MutantStack<char>::iterator begin = mutant_stack.begin();
+	MutantStack<char>::iterator end = mutant_stack.end();
+	++begin;
+	--begin;
+	while (begin != end)
 	{
-		std::cout << "Iterator: " << *it << std::endl;
-		++it;
+		std::cout << "Iterator: " << *begin << std::endl;
+		++begin;
 	}
-	std::stack<int> s(mstack);
+	std::stack<char> s(mutant_stack);
 }
 
-void TestFromSubjectWithList(void)
+void TestFromSubjectWithListChar(void)
 {
-	std::cout << BOLD YELLOW "Test from subject with list\n" OFF;
+	std::cout << BOLD YELLOW "Test from subject with list (char)\n" OFF;
 
-	std::list<int> mlist;
-	mlist.push_back(5);
-	mlist.push_back(17);
-	std::cout << "Top: " << mlist.back() << std::endl;
-	mlist.pop_back();
-	std::cout << "Size: " << mlist.size() << std::endl;
-	mlist.push_back(3);
-	mlist.push_back(5);
-	mlist.push_back(737);
-	//[...]
-	mlist.push_back(0);
-	std::list<int>::iterator it = mlist.begin();
-	std::list<int>::iterator ite = mlist.end();
-	++it;
-	--it;
-	while (it != ite)
+	std::list<char> list;
+	list.push_back('a');
+	list.push_back('Z');
+	std::cout << "Top: " << list.back() << std::endl;
+	list.pop_back();
+	std::cout << "Size: " << list.size() << std::endl;
+	list.push_back('Q');
+	list.push_back('@');
+	list.push_back('|');
+	list.push_back('0');
+	std::list<char>::iterator begin = list.begin();
+	std::list<char>::iterator end = list.end();
+	++begin;
+	--begin;
+	while (begin != end)
 	{
-		std::cout << "Iterator: " << *it << std::endl;
-		++it;
+		std::cout << "Iterator: " << *begin << std::endl;
+		++begin;
 	}
-	std::list<int> s(mlist);
+	std::list<char> s(list);
+}
+
+void TestFromSubjectWithMutantInt(void)
+{
+	std::cout << BOLD YELLOW "Test from subject with mutant stack (int)\n" OFF;
+
+	MutantStack<int> mutant_stack;
+	mutant_stack.push(5);
+	mutant_stack.push(17);
+	std::cout << "Top: " << mutant_stack.top() << std::endl;
+	mutant_stack.pop();
+	std::cout << "Size: " << mutant_stack.size() << std::endl;
+	mutant_stack.push(3);
+	mutant_stack.push(5);
+	mutant_stack.push(737);
+	mutant_stack.push(0);
+	MutantStack<int>::iterator begin = mutant_stack.begin();
+	MutantStack<int>::iterator end = mutant_stack.end();
+	++begin;
+	--begin;
+	while (begin != end)
+	{
+		std::cout << "Iterator: " << *begin << std::endl;
+		++begin;
+	}
+	std::stack<int> s(mutant_stack);
+}
+
+void TestFromSubjectWithListInt(void)
+{
+	std::cout << BOLD YELLOW "Test from subject with list (int)\n" OFF;
+
+	std::list<int> list;
+	list.push_back(5);
+	list.push_back(17);
+	std::cout << "Top: " << list.back() << std::endl;
+	list.pop_back();
+	std::cout << "Size: " << list.size() << std::endl;
+	list.push_back(3);
+	list.push_back(5);
+	list.push_back(737);
+	list.push_back(0);
+	std::list<int>::iterator begin = list.begin();
+	std::list<int>::iterator end = list.end();
+	++begin;
+	--begin;
+	while (begin != end)
+	{
+		std::cout << "Iterator: " << *begin << std::endl;
+		++begin;
+	}
+	std::list<int> s(list);
 }
 
 void TestMutantStack(void)
@@ -102,15 +152,47 @@ void TestRealStack(void)
 	std::cout << "Size: " << stack.size() << std::endl;
 }
 
+void TestConstructorDestructor(void)
+{
+	std::cout << BOLD YELLOW "\nTesting constructor and destructor\n" OFF;
+	
+	MutantStack<int> stack;
+	stack.push(11);
+
+	MutantStack<int> copy1(stack);
+
+	MutantStack<int> copy2;
+	copy2 = stack;
+	
+	std::cout << "stack top = " << stack.top() << std::endl;
+	std::cout << "copy1 top = " << copy1.top() << std::endl;
+	std::cout << "copy2 top = " << copy2.top() << std::endl;
+
+	std::cout << BOLD YELLOW "\nTesting deep copy\n" OFF;
+
+	stack.push(12);
+	copy1.push(13);
+	copy2.push(14);
+
+	std::cout << "stack top = " << stack.top() << std::endl;
+	std::cout << "copy1 top = " << copy1.top() << std::endl;
+	std::cout << "copy2 top = " << copy2.top() << std::endl;
+}
+
 int main(void)
 {
+	TestConstructorDestructor();
 	std::cout << std::endl;
 	TestRealStack();
 	std::cout << std::endl;
 	TestMutantStack();
 	std::cout << std::endl;
-	TestFromSubjectWithList();
+	TestFromSubjectWithListInt();
 	std::cout << std::endl;
-	TestFromSubjectWithMutant();
+	TestFromSubjectWithMutantInt();
 	std::cout << std::endl;
+	TestFromSubjectWithListChar();
+	std::cout << std::endl;
+	TestFromSubjectWithMutantChar();
+	return (EXIT_SUCCESS);
 }
