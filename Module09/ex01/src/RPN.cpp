3bbc8b467ddfc20d015f86c2b370dcab6f61c538
Felicia Koolhoven <fmkoolhoven@gmail.com>
2024-03-04 15:28:33 +0100

    Fixed straggler input

--- Module09/ex01/src/RPN.cpp
+++ Module09/ex01/src/RPN.cpp
@@ -6,7 +6,7 @@
 /*   By: fkoolhov <fkoolhov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/27 18:03:14 by fkoolhov          #+#    #+#             */
-/*   Updated: 2024/03/04 14:45:37 by fkoolhov         ###   ########.fr       */
+/*   Updated: 2024/03/04 15:28:00 by fkoolhov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 RPN::RPN(std::string input)
 {
+	bool straggler = false;
 	bool error = false;
 	std::istringstream input_stream(input);
 	std::string line;
@@ -23,6 +24,12 @@ RPN::RPN(std::string input)
 		try
 		{
 			stack.push(std::stoi(line));
+			if (stack.size() > 2)
+			{
+				std::cerr << RED "Invalid input\n" OFF;
+				error = true;
+			}
+			straggler = true;
 		}
 		catch (const std::exception& exception)
 		{
@@ -33,6 +40,7 @@ RPN::RPN(std::string input)
 			}
 			else
 			{
+				straggler = false;
 				int second = stack.top();
 				stack.pop();
 				int first = stack.top();
@@ -59,8 +67,10 @@ RPN::RPN(std::string input)
 			}
 		}
 	}
-
-	if (!error)
+	
+	if (straggler)
+		std::cerr << RED "Invalid input\n" OFF;
+	else if (!error)
 		std::cout << "Result: " << stack.top() << std::endl;
 }
 
